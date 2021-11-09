83f2bd7d847cc1408f556903424168c93f3e8d63
Aakash Singh <aakashsingh@Aakashs-MacBook-Air.local>
2021-11-09 23:08:15 +0530

    initial commit

---
+++ block_chain.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 19:07:21 2021
+
+@author: aakashsingh
+
+Block Chain Implementation 
+
+"""
+
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+
+class BlockChain:
+    
+    def __init__(self):
+        self.chain=[]
+        self.create_block(proof =1 , previous_hash = '0')
+        self.block_difficuilty = 4  #number of leading Zeros
+        
+    
+    def create_block(self, proof , previous_hash):
+        
+        block = {
+                'index' : len(self.chain)+1,
+                'timestamp' : str(datetime.datetime.now()),
+                'proof' : proof, #simply nonce number
+                'previous-hash' : previous_hash
+                
+            }
+        
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        
+        return self.chain[-1]
+    
+    def proof_of_work(self,previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            
+            if(hash_operation[:self.block_difficuilty] == '0'*self.block_difficuilty):
+                check_proof = True
+            else:
+                new_proof+=1
+        
+        return new_proof
+    
+    def hash(self , block):
+        
+        encoded_block = json.dumps(block , sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    
+    
+    def is_chain_valid(self,chain):
+        previous_block = self.chain[0]
+        block_index = 1
+        
+        while block_index < len(self.chain):
+            
+            block = chain[block_index]
+            if(block['previous-hash'] != self.hash(previous_block)):
+                return False
+            
+            
+            previous_proof = previous_block['proof']
+            proof  = block['proof']
+            
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            
+            if(hash_operation[:self.block_difficuilty] == '0'*self.block_difficuilty):
+                return False
+            
+            previous_block = block
+            block_index+=1
+        
+        return True
+            
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+blockchain = BlockChain()
+
+
+@app.route('/mineblock', methods=['GET'])
+def mine_block():
+    
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof , previous_hash)
+    
+    response = {
+        'message':"Congrulations you have mined sucessfully!",
+        'index' : block['index'],
+        'timestamp' : block['timestamp'],
+        'proof' : block['proof'],
+        'previous-hash' : block['previous-hash']
+        }
+    return jsonify(response),200
+
+@app.route('/getchain', methods=['GET'])
+def get_chain():
+    
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+        }
+    
+    return jsonify(response),200
+
+app.run(host = '0.0.0.0' , port =5000)
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Simple Block Chain Implementation Using Python
+
+## 1.Installation:
+- clone this repository
+- navigate to the block_chain.py file
+- run ```python3 block_chain.py```
+
+## 2.Post Man Requests:
+- url : ```http://127.0.0.1:5000/```
+- GET Requests
+- To mine new block use url   : ```http://127.0.0.1:5000/mineblock```
+- To list block chain use url : ```http://127.0.0.1:5000/getchain```
